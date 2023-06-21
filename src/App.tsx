d6a64709131abcd93adcba0daec193204f99813c
Emir Marat <emmarrat@gmail.com>
2023-06-21 20:21:46 +0600

    Removed not needed package and left message in comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import ChatWidget from "./components/ChatWidget/ChatWidget";
 function App() {
   return (
 <div className="container">
-  <div>
     <ChatWidget
       url='https://cors-anywhere.herokuapp.com/https://emmarrat.app.n8n.cloud/webhook/chat-widget'
+      // Забыл рассказать на видео в loom что использовал cors-anywhere чтобы обойти cors ошибки,
+      // так как не смог подружить n8n с localhost:3000, много читал, везде говорилось, что это можно настроить легко когда n8n запущен на своем сервере
+      // Чтобы протеститровать нужно будет зайти на ссылку https://cors-anywhere.herokuapp.com
+      // и получить доступ к cors, после чего запросы на апи будут проходить без проблем
+      emptyMessages="Send me message to start"
+      minHeight={100}
+      maxHeight={550}
     />
-  </div>
 </div>
   );
 }
